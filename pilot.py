d0d50397d9be8fc5f4e93b2811636106d71de6df
Egsy05 <162001169+Egsy05@users.noreply.github.com>
2024-06-12 18:09:12 +0700

    Update pilot.py

--- pilot.py
+++ pilot.py
@@ -668,12 +668,12 @@ elif selected == "Find":
 
         # st.scatter_chart(filter_table, x='Positive_Negative_dif', y='Peak CCU', color='Name',size='days_since_release')
     
-    if customer_type == "Recommended":
+    elif customer_type == "Recommended":
         data = pd.read_csv("genres.csv")
         data1 = pd.read_csv("games_.csv")
         data1['Genres'] = data['Genres']
         # data1['About the game'] = data['About the game']
-        data1 = data1[data1["Estimate users"]>=5000].reset_index()
+        # data1 = data1[data1["Estimate users"]>=5000].reset_index()
 
         st.header("Let the Machine Decide!", divider='blue')
         data_game = data1.Name.unique()
